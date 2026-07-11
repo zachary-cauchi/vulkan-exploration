0263f64e3c7427c7af85a9148214f477f775de1e
Zachary Cauchi <zach.cauchi@hotmail.com>
2026-07-11 14:31:20 +0200

    feat: Print queue families and capabilities

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,9 @@
-use tracing::{debug, info, warn};
+use std::{collections::HashMap, sync::Arc};
+
+use tracing::{debug, info, instrument};
 use vulkano::{
     VulkanLibrary,
+    device::{QueueFlags, physical::PhysicalDevice},
     instance::{Instance, InstanceCreateInfo},
 };
 
@@ -14,6 +17,12 @@ pub fn run() -> CrateResult<()> {
 
     info!("Enumerating physical devices.");
 
+    print_devices_info(&vk_instance)?;
+
+    Ok(())
+}
+
+fn print_devices_info(vk_instance: &Arc<Instance>) -> CrateResult<()> {
     let mut devices = vk_instance.enumerate_physical_devices()?.peekable();
 
     if devices.peek().is_none() {
@@ -21,28 +30,62 @@ pub fn run() -> CrateResult<()> {
     }
 
     for device in devices {
-        let name = &device.properties().device_name;
-        let device_type = device.properties().device_type;
-        let driver_name = device
-            .properties()
-            .driver_info
-            .as_ref()
-            .map_or("Not found", |s| s.as_str());
-        let max_api = device.api_version();
-
-        debug!(
-            "Found device. Name: '{name}', Type: '{device_type:?}', driver: '{driver_name}', Max API version: '{max_api}'",
-        );
-
-        // TODO Requires 'khr_display' extension enabled in instance.
-        // match device.display_properties() {
-        //     Ok(displays) => debug!("Displays: {:?}", displays),
-        //     Err(e) => warn!(
-        //         "Could not get displays for device. Error: {}",
-        //         CrateError::from(e)
-        //     ),
-        // }
+        print_device_info(device);
     }
 
     Ok(())
+}
+
+#[instrument(name = "device", skip_all, fields(name = device.properties().device_name))]
+fn print_device_info(device: Arc<PhysicalDevice>) {
+    let device_type = device.properties().device_type;
+    let driver_name = device
+        .properties()
+        .driver_info
+        .as_ref()
+        .map_or("Not found", |s| s.as_str());
+    let max_api = device.api_version();
+
+    debug!(
+        "Found device. Type: '{device_type:?}', driver: '{driver_name}', Max API version: '{max_api}'",
+    );
+
+    let mut family_caps: HashMap<QueueFlags, (usize, usize)> = HashMap::new();
+
+    for (i, queue_family) in device.queue_family_properties().iter().enumerate() {
+        debug!(
+            "Family {i} - Queue count: {}, flags: {:?}",
+            queue_family.queue_count, queue_family.queue_flags
+        );
+
+        let caps = [
+            QueueFlags::COMPUTE,
+            QueueFlags::GRAPHICS,
+            QueueFlags::TRANSFER,
+        ];
+
+        for cap in caps {
+            if queue_family.queue_flags.intersects(cap) {
+                let entry = family_caps.entry(cap).or_default();
+                entry.0 += 1;
+                entry.1 += queue_family.queue_count as usize;
+            }
+        }
+    }
+
+    for (cap, (family_count, queue_count)) in family_caps.into_iter() {
+        debug!(
+            "{} queue families ({} total queues) support the '{:?}' feature.",
+            family_count, queue_count, cap
+        );
+    }
+
+    // TODO Requires 'khr_display' extension enabled in instance.
+    // match device.display_properties() {
+    //     Ok(displays) => debug!("Displays: {:?}", displays),
+    //     Err(e) => warn!(
+    //         "Could not get displays for device. Error: {}",
+    //         CrateError::from(e)
+    //     ),
+    // }
 }
